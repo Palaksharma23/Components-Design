51a65a463fe613d5c6ed0415c7b6d8cb14daaf3e
palaksharma23 <mahimasharma2312@gmail.com>
2022-06-09 20:46:21 +0530

    Added Files

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <ButtonComponent classname="primary margin_1" text="Click Me"/>
+  <ButtonComponent classname="primary_wh margin_1" text="Click Me"/>
+  <ButtonComponent classname="primary_woh margin_1" text="Click Me"/>
+  <ButtonComponent classname="round-tag margin_1" text="Click Me"/>
+  <ButtonComponent classname="round-tag_woh margin_1" text="Click Me"/>
+  <ButtonComponent classname="round-tag_wh margin_1" text="Click Me"/>
+  <ButtonComponent classname="turquoise_green margin_1" text=">"/>
+</template>
+
+<script> 
+import ButtonComponent from './components/ButtonComponent.vue'
+
+export default {
+  name: 'App',
+  components: {
+    ButtonComponent
+  }
+}
+</script>
+
+<style> 
+.margin_1 {
+  margin: 1%;
+}
+</style>
