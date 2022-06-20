1acc8007b8bb8838e128e14a17369168198ea65b
Janvi <janvithakkar.583@gmail.com>
2022-06-21 01:42:00 +0530

    Added transition effect in accordian component

--- src/components/Accordion/AccordionComponent.vue
+++ src/components/Accordion/AccordionComponent.vue
@@ -1,36 +1,38 @@
 <template>
-  <div :class="classname">
-    <slot></slot>
-  </div>
+    <div :class="classname">
+        <slot></slot>
+    </div>
 </template>
 
 <script>
-export default {
-  name: "AccordionComponent",
-  props: {
-    classname: String,
-  },
-};
+    export default {
+        name: "AccordionComponent",
+        props: {
+            classname: String,
+        },
+    };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-.center {
-  margin-left: auto;
-  margin-right: auto;
-}
-.container80 {
-  width: 80vw;
-}
-.container60 {
-  width: 60vw;
-}
+    .center {
+        margin-left: auto;
+        margin-right: auto;
+    }
 
-.container40 {
-  width: 40vw;
-}
+    .container80 {
+        width: 80vw;
+    }
 
-.container20 {
-  width: 20vw;
-}
+    .container60 {
+        width: 60vw;
+    }
+
+    .container40 {
+        width: 40vw;
+    }
+
+    .container20 {
+        width: 20vw;
+    }
 </style>
